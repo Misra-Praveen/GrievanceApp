d7c494f75242f6ce1f6639c44173da82ad30efc7
Misra-Praveen <vgipra@gmail.com>
2025-12-07 02:21:46 +0530

    tailwind css installation set-up

--- grievanceFrontend/src/App.jsx
+++ grievanceFrontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-        <h1>Grievance Project Set-Up Initalizaton</h1>
+        <h1 className="text-3xl font-bold">Grievance Project Set-Up Initalizaton</h1>
       
     </>
   )
