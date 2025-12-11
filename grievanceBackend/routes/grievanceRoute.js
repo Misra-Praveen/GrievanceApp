b5b04a31506a6c2b0a1e3f490805bb87854b1941
Misra-Praveen <vgipra@gmail.com>
2025-12-11 22:21:33 +0530

    Enhance grievance Route

--- grievanceBackend/routes/grievanceRoute.js
+++ grievanceBackend/routes/grievanceRoute.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { createGrievance, getAllGrievance, getGrievanceByGrievanceNo } from "../controllers/grievanceController.js";
+import {protect} from '../middlewares/authMiddleware.js'
 
 const grievanceRouter = Router();
 
 
 grievanceRouter.post("/registerGrievance", createGrievance);
 grievanceRouter.get("/viewStatus", getGrievanceByGrievanceNo);
-grievanceRouter.get("/admin/", getAllGrievance)
+grievanceRouter.get("/adminGrievances/",protect, getAllGrievance)
 
 export default grievanceRouter
