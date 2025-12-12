b7fd7a1383a2bba0195f1df8cf23783cc65a8fbc
Misra-Praveen <vgipra@gmail.com>
2025-12-12 06:12:15 +0530

    Fixed bug

--- grievanceBackend/middlewares/permissionMiddleware.js
+++ grievanceBackend/middlewares/permissionMiddleware.js
@@ -2,7 +2,7 @@
 
 const checkPermission = (permission)=>{
     return (req,res, next) => {
-        if(!req.user || !req.user.role || !req.user.role.permission.includes(permission)){
+        if(!req.user || !req.user.role || !req.user.role.permissions.includes(permission)){
             return res.status(403).json({ message: "Access denied" });
         }
         next();
